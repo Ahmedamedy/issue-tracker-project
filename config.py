09579c1dcc3751336aa176213556b5600ce2413b
Ahmed Yusuf <Ahmedamedy@gmail.com>
2017-02-16 13:20:59 +0300

    Adding admin templates

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from db_setup import Base, Issues, Users
+from db_setup import Base, Issues, Users, CompanyUsers
 
 engine = create_engine('sqlite:///issue-tracker.db')
 
@@ -70,7 +70,220 @@ def newissue():
         return redirect(url_for('issues'))
     else:
         return render_template('newissue.html')	
-        
+		
+@app.route('/admin/login', methods=['GET','POST'])
+def adminlogin():
+    if request.method == 'POST':
+            if sessions.query(CompanyUsers).filter_by(username =request.form['username'], password=request.form['password'], designation=request.form['designation']).count() < 1:
+                flash("Wrong Username or Password")
+                return redirect(url_for('adminlogin'))
+            else:
+                session['username'] = request.form['username']
+                user = sessions.query(CompanyUsers).filter_by(username =request.form['username'], password=request.form['password'], designation=request.form['designation']).one()
+                session['id'] = user.id
+				
+                return redirect(url_for('adminissues'))
+    else:
+        return render_template('adminlogin.html')
+
+
+@app.route('/admin/issues')
+def adminissues():
+    if 'username' in session:
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(department=user.department)
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department, designation="personel")
+        return render_template('adminissues.html', items=items, user = user, personel=personel)
+    else:
+        return redirect(url_for('home'))
+
+@app.route('/admin/issues/opened')
+def openedissues():
+    if 'username' in session:
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(department=user.department, opened = 0)
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department, designation="personel")
+        return render_template('openedissues.html', items=items, user = user, personel=personel)
+    else:
+        return redirect(url_for('home'))
+    
+@app.route('/admin/issues/closed')
+def closedissues():
+    if 'username' in session:
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(department=user.department, opened= 1)
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department, designation="personel")
+        return render_template('closedissues.html', items=items, user = user, personel=personel)
+    else:
+        return redirect(url_for('home'))
+
+@app.route('/admin/issues/high')
+def highissues():
+    if 'username' in session:
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(department=user.department, priority = "High")
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department, designation="personel")
+        return render_template('openedissues.html', items=items, user = user, personel=personel)
+    else:
+        return redirect(url_for('home'))
+
+@app.route('/admin/issues/critical')
+def criticalissues():
+    if 'username' in session:
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(department=user.department, priority = "Critial")
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department, designation="personel")
+        return render_template('openedissues.html', items=items, user = user, personel=personel)
+    else:
+        return redirect(url_for('home'))
+    
+
+@app.route('/admin/comment/issue/<int:item_id>', methods=['GET','POST'])
+def commentissue(item_id):
+    editeditem = sessions.query(Issues).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        remarks = request.form['remarks']
+        editeditem.adminremarks = remarks
+        sessions.add(editeditem)
+        sessions.commit()
+        flash("item updated")
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(assignned=user.username)
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department, designation="personel")
+        return redirect(url_for('adminissues'))
+    else:
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(department=user.department)
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department, designation="personel")
+        return render_template('commentissue.html', item_id=item_id, i=editeditem, user = user, personel=personel )
+
+
+    
+@app.route('/admin/assign/issue/<int:item_id>', methods=['GET','POST'])
+def assignissue(item_id):
+    editeditem = sessions.query(Issues).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        assignned = request.form['assign']
+        editeditem.assignned = assignned
+        sessions.add(editeditem)
+        sessions.commit()
+        flash("item updated")
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(department=user.department)
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department, designation="personel")
+        return redirect(url_for('adminissues'))
+    else:
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(department=user.department)
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department, designation="personel")
+        return render_template('assignissue.html', item_id=item_id, i=editeditem, user = user, personel=personel )
+
+
+
+
+@app.route('/admin/update/issue/<int:item_id>', methods=['GET','POST'])
+def updateissue(item_id):
+    editeditem = sessions.query(Issues).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        resolved = request.form['resolved']
+        editeditem.resolved = resolved
+        sessions.add(editeditem)
+        sessions.commit()
+        flash("item updated")
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(department=user.department)
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department)
+        return redirect(url_for('adminissues'))
+    else:
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(department=user.department)
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department)
+        return render_template('updateissue.html', item_id=item_id, i=editeditem, user = user, personel=personel )
+
+@app.route('/staff/logout')
+def stafflogout():
+   session.pop('username', None)
+   return redirect(url_for('stafflogin'))
+   
+		
+@app.route('/staff/login', methods=['GET','POST'])
+def stafflogin():
+    if request.method == 'POST':
+            if sessions.query(CompanyUsers).filter_by(username =request.form['username'], password=request.form['password']).count() < 1:
+                flash("Wrong Username or Password")
+                return redirect(url_for('stafflogin'))
+            else:
+                session['username'] = request.form['username']
+                user = sessions.query(CompanyUsers).filter_by(username =request.form['username'], password=request.form['password']).one()
+                if user.designation == "admin":
+                    session['username'] = request.form['username']
+                    user = sessions.query(CompanyUsers).filter_by(username =request.form['username'], password=request.form['password']).one()
+                    session['id'] = user.id		
+                    return redirect(url_for('adminissues'))
+                else:
+                    session['id'] = user.id
+                    return redirect(url_for('staffissues'))             
+    else:
+        return render_template('stafflogin.html')
+
+
+@app.route('/staff/issues')
+def staffissues():
+    if 'username' in session:
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(assignned=user.username)
+        return render_template('staffissues.html', items=items)
+    else:
+        return redirect(url_for('stafflogin'))
+
+@app.route('/staff/update/issue/<int:item_id>', methods=['GET','POST'])
+def staffupdateissue(item_id):
+    editeditem = sessions.query(Issues).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        comments = request.form['comments']
+        editeditem.remarks = comments
+        sessions.add(editeditem)
+        sessions.commit()
+        flash("item updated")
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        items = sessions.query(Issues).filter_by(department=user.department)
+        personel = sessions.query(CompanyUsers).filter_by(department=user.department)
+        return redirect(url_for('staffissues'))
+    else:
+        username = session['username']
+        user_id = session['id']
+	user = sessions.query(CompanyUsers).filter_by(id = user_id).one()
+        return render_template('staffupdateissue.html', item_id=item_id, i=editeditem )
+
+
+
+
 if __name__ == '__main__':
     app.secret_key = "secret_key"
     app.debug = True
