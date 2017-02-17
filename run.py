42269c2456332f4cd1c045c779d362de44277bf3
Ahmed Yusuf <Ahmedamedy@gmail.com>
2017-02-17 08:29:02 +0300

    Port Changes

--- run.py
+++ run.py
@@ -288,5 +288,5 @@ def staffupdateissue(item_id):
 
 if __name__ == '__main__':
     app.secret_key = "secret_key"
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
